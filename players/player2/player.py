cfb14a26af87242879b87ee16595d03ce103274a
Hibatullah Shaalan <hshaalan@oberlin.edu>
2025-10-06 15:15:52 -0400

    hiba's branch

--- players/player2/player.py
+++ players/player2/player.py
@@ -9,4 +9,14 @@ class Player2(Player):
         super().__init__(children, cake, cake_path)
 
     def get_cuts(self) -> list[tuple[Point, Point]]:
-        return []
+        cuts = []
+        for _ in range(self.children):
+            largest_piece = max(self.cake.get_pieces(), key=lambda piece: piece.area)
+            from_p = largest_piece.centroid
+            to_p = Point(largest_piece.exterior.coords[0] + largest_piece.exterior.coords[-1]) / 2
+            is_valid, _ = self.cake.cut_is_valid(from_p, to_p)
+            if is_valid:
+                cuts.append((from_p, to_p))
+                self.cake.cut(from_p, to_p)
+        return cuts
+
